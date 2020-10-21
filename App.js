5bd37dedd09078cb16bb0cd7eda5c2a2ea650075
akshitone <akshitone8@gmail.com>
2020-10-21 18:57:51 +0530

    setup to do list in rn app

--- App.js
+++ App.js
@@ -1,30 +1,53 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working on your app!"
-  );
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [yourGoals, setYourGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // here ...yourGoals is previous goals that we already inserted and entered goal is new goal that we are inserted latest
+    setYourGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Click me to change"
-        onPress={() => {
-          setOutputText("Hello I'm Akshit Mithaiwala");
-        }}
-      />
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="What's your new goal?"
+          style={styles.inputText}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {yourGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  inputText: {
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 10,
+    width: "70%",
   },
 });
